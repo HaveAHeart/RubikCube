a12276c2459aad3f46b5280fb19e4e40173b5739
Alexey <bedrin18@mail.ru>
2019-02-16 21:46:44 +0300

    Side class: -methods changeRow/changeCol for side rotation realisation Cube class: -added simple generator(too simple :c) --Side orders in array: top -> front -> right -> back -> left -> bottom -added rotateRow method --we can set rotation side, amount of rotated(from startNum to endNum in a row only) rows and direction(CW/CCW) --really long code for rotating top/bottom rows
    all information now in Readme-file in repository
    
    TODO:
    rotateCol method in Cube class
    -some fragments of code can be taken from rotateRow method
    Cube generator

--- src/main/Rubik.java
+++ src/main/Rubik.java
@@ -1,178 +1,146 @@
 package main;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-
-/*
-Cube -> 6 sides. Every side consists of 3 rows OR 3 columns.
-Layers spin realisation can be created using rows ans columns.
-
-clockwise rotation - short to CW
-counterclockwise - same way to CCW
-
-rotating CW-> top row becomes right column, middle - to middle, so on
-
-111                               1                                   2
-222 -> rotating row 111 to column 1 , placing it to the right, 222 -> 2 , so on
-333                               1                                   2
-
-connected sides rotation can also be realised with replacing rows
-
-different sizes can be realised this way - just doing some amount of iterations
-
-requires some Row/Column rotating methods(clockwise/counterclockwise)
-
-TODO:
-class Row
--rotateRight/rotateLeft methods(transforming to Column)
--way to save data(triplets(-, no size variations), arrays(maybe))
--constructor - solve size problems(4x4x4 cube, how to apply only 4 elements arrays)
-
-class Column
--rotateRight/rotateLeft methods(transforming to Row)
--way to save data(triplets(-, no size variations), arrays(maybe))
--constructor - solve size problems(4x4x4 cube, how to apply only 4 elements arrays)
-
-class Side
--getting 1st/2nd/3rd Row/Column (or just an array of rows/columns)
--constructor - creating Side from 3 Rows or 3 Columns
-
-class Cube
--side state method (just printing it, returning an array)
--rotation methods(CW/CCW, dumb way - CCW spin = 3 CW spins)
---detecting if outer side is rotated - if it is,change that side
-    as was written before
---rotate connected rows/columns in connected sides - just replace them
-    (depends on rotating direction)
----need a way to somehow realise which sides are connected, also - names of sides
-    (I think, just name them after their colors is enough - colors are constantly connected in Rubik Cubes)
--change current side - whole cube rotation
--constructor - create Cube from 6 Sides
-
-random SOLVING cube generation
--8 parts with 3 colors
--6 centers - need to place them in pair by their color (only in odd sizes)
--(N-2)*8 parts with 2 colors - (N-2)*4 on opposite sides
--(N*N*6)-8*3-(N-2)*8*2 parts with 1 color (all the parts without 3-colored parts and without 2-colored parts,
-    because I want to generate and to set them firstly) (DO NOT FORGET ABOUT CENTERS!!!!)
--3-colored - constant things, 2-colored - constant(constant border sides), 1-colored - amount/6 for each color
--generate 3-colored
---1st and last element of first and last Row on each side should be colored during this period
--generate 2-colored
---1st and last Row and 1st and last Columns on every side should be generated
--generate 1-colored
---center disposition for odd sizes
---filling all the uncolored spaces with the rest of colors
--returning Cube with random generation
- -OR-
--just rotate sides in already solved cube random amount of times in random directions, lmao
-
-solving 3x3 Cube
--creating white cross
--creating white corners
--creating middle layer
--creating yellow cross
--creating yellow corners
--orientating yellow corner
--arrangement of yellow edges color
-
- */
 public class Rubik {
     int dim;
-    public Rubik (int dimensions) {
+
+    public Rubik(int dimensions) {
         dim = dimensions;
     }
+
     public class Row {
         String values;
+
         //generator
-        public Row(String str) { values = str; } //TODO: str length check
+        public Row(String str) {
+            values = str;
+        } //TODO: str length check
+
         //------------------
         //rotation methods
-        public Col rotateCW() { return new Col(values); }
+        public Col rotateCW() {
+            return new Col(values);
+        }
+
         public Col rotateCCW() {
             values = new StringBuilder(values).reverse().toString();
             return new Col(values);
         }
+
         //------------------
         //access methods
-        public char getChar(int num) { return values.charAt(num); } //TODO: num check
+        public char getChar(int num) {
+            return values.charAt(num);
+        } //TODO: num check
     }
 
     public class Col {
         String values;
+
         //generator
         public Col(String str) {
             values = str;
         } //TODO: str length check
+
         //------------------
         //rotation methods
         public Row rotateCW() {
-            return new Row(values);
-        }
-        public Row rotateCCW() {
             values = new StringBuilder(values).reverse().toString();
             return new Row(values);
         }
+
+        public Row rotateCCW() {
+            return new Row(values);
+        }
+
         //------------------
         //access methods
-        public char getChar(int num) { return values.charAt(num); } //TODO: num check
+        public char getChar(int num) {
+            return values.charAt(num);
+        } //TODO: num check
     }
 
     public class Side {
         String values = "";
         StringBuilder sb = new StringBuilder();
+
         //generators
-        public Side (Col[] cols) { //TODO: check amount and size of columns
+        public Side(Col[] cols) { //TODO: check amount and size of columns
             for (int i = 0; i < dim; i++) {
-                for (int b = 0; b < dim; b++) { sb.append(cols[b].getChar(i)); }
+                for (int b = 0; b < dim; b++) {
+                    sb.append(cols[b].getChar(i));
+                }
             }
             values = sb.toString();
         }
-        public Side (Row[] rows) { //TODO: check amount and size of rows
+
+        public Side(Row[] rows) { //TODO: check amount and size of rows
             for (int i = 0; i < dim; i++) {
-                for (int b = 0; b < dim; b++) { sb.append(rows[i].getChar(b)); }
+                for (int b = 0; b < dim; b++) {
+                    sb.append(rows[i].getChar(b));
+                }
             }
             values = sb.toString();
         }
-        public Side (String str) {
+
+        public Side(String str) {
             values = str;
         }
+
         //------------------
         //side parts access methods
-        public Row getRow (int rowNum) { //TODO: check rowNum
+        public Row getRow(int rowNum) { //TODO: check rowNum
             StringBuilder sb = new StringBuilder();
             for (int i = rowNum * (dim - 1) - 1; i < rowNum * dim - 1; i++) {
                 sb.append(values.charAt(i));
             }
             return new Row(sb.toString());
         }
-        public Col getCol (int colNum) { //TODO: check colnum
+
+        public Col getCol(int colNum) { //TODO: check colnum
             StringBuilder sb = new StringBuilder();
             for (int i = colNum - 1; i < dim * dim; i += dim) {
                 sb.append(values.charAt(i));
             }
             return new Col(sb.toString());
         }
+
+        public Side changeRow(int rowNum, Row replace) {
+            StringBuilder sb = new StringBuilder(values);
+            values = sb.replace(dim * rowNum, dim * (rowNum + 1), replace.values).toString();
+            return this;
+        }
+
+        public Side changeCol(int colNum, Col replace) {
+            StringBuilder sb = new StringBuilder(values);
+            for (int i = 0; i < dim; i++) {
+                sb.replace(colNum + (dim * i), colNum + (dim * i) + 1,
+                        Character.toString(replace.values.charAt(i)));
+            }
+            values = sb.toString();
+            return this;
+        }
+
         //------------------
         //rotation methods
-        public Side rotateCW () {
+        public Side rotateCW() {
             Col[] cols = new Col[dim];
-            for (int i = dim - 1; i >= 0; i--) {/*TODO: fix this crap, access modifying obj*/
-                cols[i] = new Side(values).getRow(dim - i).rotateCW(); //terrible
+            for (int i = dim - 1; i >= 0; i--) {
+                cols[i] = this.getRow(dim - i).rotateCW();
             }
             return new Side(cols);
         }
-        public Side rotateCCW () {
+
+        public Side rotateCCW() {
             Col[] cols = new Col[dim];
-            for (int i = 0; i < dim; i++) {/*TODO: fix this crap, access modifying obj*/
-                cols[i] = new Side(values).getRow(i).rotateCCW(); //terrible
+            for (int i = 0; i < dim; i++) {
+                cols[i] = this.getRow(i).rotateCCW();
             }
             return new Side(cols);
         }
+
         //------------------
         //side state access methods
-        public void sideState () { //TODO: maybe, some array-returning realisation?
+        public void sideState() { //TODO: maybe, some array-returning realisation?
             for (int i = 0; i < dim; i++) {
                 StringBuilder sb = new StringBuilder();
                 for (int b = 0; b < dim; b++) {
@@ -185,7 +153,120 @@ public class Rubik {
 
         }
     }
-    public class Cube {
 
+    public class Cube {
+        Side[] values = new Side[6]; //sides arr format: top - front - right - back - left - bot sides
+        String[] topBotRowRot = {"right", "bottom", "left", "top"}; //CW rotation - order for rotating rows/cols
+        String[] topBotColRot = {"front", "bottom", "back", "top"}; //for CCW rotation need to reverse the order
+        String[] rightLeftRowRot = {"back", "left", "front", "right"};
+        String[] rightLeftColRot = {"bottom", "left", "top", "right"};
+        String[] frontBackRowRot = {"right", "back", "left", "front"};
+        String[] frontBackColRot = {"bottom", "back", "top", "front"};
+
+        //generators
+        public Cube(Side[] sides) {
+            values = sides;
+        }
+
+        //------------------
+        //rotation methods
+        public void rotateRow(String side, String direction, int startNum, int endNum) {
+            if (side.toLowerCase().equals("top") || side.toLowerCase().equals("bottom")) {
+                if (direction.toLowerCase().equals("cw")) {
+                    for (int rowNum = startNum; rowNum <= endNum; rowNum++) {
+                        if (rowNum == 0) { //rotating whole affected front side
+                            if (side.toLowerCase().equals("top")) { values[3] = values[3].rotateCCW(); }
+                            else { values[1] = values[1].rotateCW(); }
+                        } else {
+                            if (rowNum == dim - 1) { //rotating whole affected back side
+                                if (side.toLowerCase().equals("top")) { values[1] = values[1].rotateCW(); }
+                                else { values[3] = values[3].rotateCCW(); }
+                            }
+                        }
+                        Row replacingRow = values[4].getCol(dim - rowNum).rotateCW();
+                        Row tempRow = values[0].getRow(rowNum); //rotating - taking row at the top side
+                        values[0] = values[0].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[2].getCol(dim - rowNum).rotateCW();
+                        values[2] = values[2].changeCol(dim - rowNum, replacingRow.rotateCW());
+                        replacingRow = tempRow;
+                        tempRow = values[5].getRow(dim - rowNum);
+                        values[5] = values[5].changeRow(dim - rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        values[4] = values[4].changeCol(dim - rowNum, replacingRow.rotateCW());
+                    }
+                } else {
+                    for (int rowNum = startNum; rowNum <= endNum; rowNum++) {
+                        //rotating whole affected front/back side
+                        if (rowNum == 0) {
+                            if (side.toLowerCase().equals("top")) { values[3] = values[3].rotateCCW(); }
+                            else { values[1] = values[1].rotateCW(); }
+                        } else {
+                            if (rowNum == dim - 1) {
+                                if (side.toLowerCase().equals("top")) { values[1] = values[1].rotateCW(); }
+                                else { values[3] = values[3].rotateCCW(); }
+                            }
+                        }
+                        //------------------
+                        //replacing row - column - row - column
+                        Row replacingRow = values[2].getCol(dim - rowNum).rotateCCW();
+                        Row tempRow = values[0].getRow(rowNum); //rotating - taking row at the top side
+                        values[0] = values[0].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[4].getCol(dim - rowNum).rotateCCW();
+                        values[4] = values[4].changeCol(dim - rowNum, replacingRow.rotateCCW());
+                        replacingRow = tempRow;
+                        tempRow = values[5].getRow(dim - rowNum);
+                        values[5] = values[5].changeRow(dim - rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        values[4] = values[4].changeCol(dim - rowNum, replacingRow.rotateCCW());
+                    }
+                }
+            } else {
+                if (direction.toLowerCase().equals("cw")) {
+                    for (int rowNum = startNum; rowNum <= endNum; rowNum++) {
+                        if (rowNum == 0) { values[0] = values[0].rotateCW(); }
+                        else {
+                            if (rowNum == dim - 1) { values[5] = values[5].rotateCCW(); }//top/bottom side
+                        }
+                        Row replacingRow = values[4].getRow(rowNum);
+                        Row tempRow = values[1].getRow(rowNum);
+                        values[1] = values[1].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[2].getRow(rowNum);
+                        values[2] = values[2].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[3].getRow(rowNum);
+                        values[3] = values[3].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        values[4] = values[4].changeRow(rowNum, replacingRow);
+                    }
+                } else {
+                    for (int rowNum = startNum; rowNum <= endNum; rowNum++) {
+                        //rotating whole affected front/back side
+                        if (rowNum == 0) { values[0] = values[0].rotateCCW(); } //rotating whole affected
+                        else {
+                            if (rowNum == dim - 1) { values[5] = values[5].rotateCW(); } //top/bottom side
+                        }
+                        //------------------
+                        //replacing row - column - row - column
+                        Row replacingRow = values[2].getRow(rowNum);
+                        Row tempRow = values[1].getRow(rowNum);
+                        values[1] = values[1].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[4].getRow(rowNum);
+                        values[4] = values[4].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        tempRow = values[3].getRow(rowNum);
+                        values[3] = values[3].changeRow(rowNum, replacingRow);
+                        replacingRow = tempRow;
+                        values[2] = values[2].changeRow(rowNum, replacingRow);
+                    }
+                }
+            }
+        }
+        public void rotateCol(String side, String direction, int startNum, int endNum) {
+
+        }
     }
 }
